5dd73e02445b64e485b9b2897e084fad80b3eed5
TobyMike-max <osinubi.michael@gmail.com>
2022-04-18 10:39:59 +0100

    Push _printf main file

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * format_specifier - Checks if there is a valid format specifier
+ * @format: Possible format specifier
+ * Return: Pointer to valid function or NULL
+ */
+static int (*format_specifier(const char *format))(va_list)
+{
+	unsigned int i;
+	print p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - Prints anything
+ * @format: List of argument types passed to the function
+ * Return: Number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list args;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = format_specifier(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(args);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(args);
+	return (count);
+}
