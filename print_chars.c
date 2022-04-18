7899d5eaa51879c07cdddeccf6497bec8fcc18d3
Ajibola <davidajibolatimilehin@gmail.com>
2022-04-18 11:04:46 +0100

    updated print_chars.c

--- print_chars.c
+++ print_chars.c
@@ -12,3 +12,24 @@ int print_c(va_list c)
 	_putchar(ch);
 	return (1);
 }
+
+/**
+ * print_s - prints a string
+ * @s: string to be printed
+ * Return: Number of chars printed
+*/
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
+}
