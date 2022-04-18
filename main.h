bfbd11632c94f1f3a6dc84c224819392f3a9086c
TobyMike-max <osinubi.michael@gmail.com>
2022-04-18 13:41:38 +0100

    Corrected line with over 80 characters

--- main.h
+++ main.h
@@ -4,7 +4,8 @@
 #include <stdarg.h>
 
 /**
- * struct print - This structure handles the format specifiers and the corresponding
+ * struct print - This structure handles the format 
+ * specifiers and the corresponding
  * function.
  * @t: type to print (format specifier)
  * @f: function to print
