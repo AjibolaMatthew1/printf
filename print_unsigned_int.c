a70b4699ba6ef4d390f34a3b8375e2a0d3b2dc5d
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-18 23:18:29 +0100

    Update print_unsigned_int.c

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -50,7 +50,7 @@ int print_u(va_list u)
 		m /= 10;
 		a[i] = (n / m) % 10;
 	}
-	for (i = 0; sum = 0; count = 0; i < 10; i++)
+	for (i = 0, sum = 0, count = 0, i < 10; i++)
 	{
 		sum += a[i];
 		if (sum || i == 9)
