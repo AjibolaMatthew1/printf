59495808a3b427f2be1aa39bfb6462ae8a9f50b0
TobyMike-max <osinubi.michael@gmail.com>
2022-04-18 14:02:52 +0100

    Removed trailing whitespace

--- main.h
+++ main.h
@@ -4,9 +4,8 @@
 #include <stdarg.h>
 
 /**
- * struct print - This structure handles the format 
- * specifiers and the corresponding
- * function.
+ * struct print - This structure handles the
+ * format specifiers and the corresponding function.
  * @t: type to print (format specifier)
  * @f: function to print
  */
