9b8eaed677bb8da94355e133c699996a0e8f8d9f
Ajibola <52489772+AjibolaMatthew1@users.noreply.github.com>
2022-04-19 18:52:21 +0100

    Update print_chars.c

--- print_chars.c
+++ print_chars.c
@@ -57,6 +57,30 @@ int print_r(va_list r)
 }
 
 /**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+/**
  * print_S - prints a string and the ascii value for non-printable characters
  * @S: string to be printed
  * Return: Number of chars printed
