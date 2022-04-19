1faa6408db8254cba41e87c9373f72e810d6f9a1
TobyMike-max <osinubi.michael@gmail.com>
2022-04-19 14:19:34 +0100

    Included code for printing reversed string

--- print_chars.c
+++ print_chars.c
@@ -18,19 +18,40 @@ int print_c(va_list c)
  * print_s - prints a string
  * @s: string to be printed
  * Return: Number of chars printed
-*/
+ */
 int print_s(va_list s)
 {
 	int count;
 	char *str = va_arg(s, char *);
 
 	if (str == NULL)
-	{
 		str = "(null)";
-	}
+
 	for (count = 0; str[count]; count++)
-	{
 		_putchar(str[count]);
+
+	return (count);
+}
+
+/**
+ * print_r - Prints a string in reverse
+ * @r: String to print
+ * Return: Number of chars printed
+ */
+int print_r(va_list r)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
 	}
 	return (count);
 }
